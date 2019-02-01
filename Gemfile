82d716e0ff345235ab4225f87cabf14b153b0bb1
Sebastian Lavender <sebastianlavender@outlook.com>
2019-02-01 09:37:34 +0000

    add time that peep was made

--- Gemfile
+++ Gemfile
@@ -2,17 +2,19 @@ source 'https://rubygems.org'
 
 ruby '2.6.0'
 
-gem 'dm-postgres-adapter'
 gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'dm-timestamps'
+gem 'pg'
 gem 'rake'
 gem 'rb-readline'
 gem 'rubocop', '0.63.1'
 gem 'sinatra'
-gem 'pg'
 
 group :test do
   gem 'capybara'
   gem 'rspec'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'timecop'
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,3 +1,5 @@
+require 'timecop'
+
 feature 'index' do
   scenario 'it should initiliaze with a welcome page' do
     visit('/')
@@ -6,10 +8,26 @@ feature 'index' do
 end
 
 feature 'add peep' do
+
+  before do
+    Timecop.freeze(Time.now)
+  end
+
+  after do
+    Timecop.return
+  end
+
   scenario 'I want to add a new peep' do
     visit('/')
     fill_in('message', with: 'rspec_test')
     click_button('Submit')
     expect(page).to have_content('rspec_test')
+  end
+
+  scenario 'I want the peep to say the time it was created' do
+    visit('/')
+    fill_in('message', with: 'rspec_test')
+    click_button('Submit')
+    expect(page).to have_content("#{Time.now.strftime("%F %T")}")
   end
 end
